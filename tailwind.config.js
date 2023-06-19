69c63b03b7b143b7c905ca670dc8c43f5efb26e3
Tiago <727320+brtiago@users.noreply.github.com>
2023-06-19 18:41:10 -0300

    feat: Atualização de cores, fontes e tamanhos

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,43 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        mulish: ['Mulish', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'color1': '#FFFFFF',
+        'color2': '#FAFAFA',
+        'color3': '#333333',
+        'color4': '#00000052',
+        'color5': '#676767',
+        'color6': '#666666',
+        'color7': '#00000000',
+        'color8': '#9F9F9F',
+        'color9': '#FA641E',
+        'color10': '#FF881F',
+        'color11': '#00000014',
+        'color12': '#FF671F',
+        'color13': '#FF671F52',
+        'color14': '#9F9F9F',
+        'color15': '#CCCCCC',
+        'color16': '#F0F0F0',
+        'color17': '#000000',
+        'color18': '#EEEEEE',
+      },
+
+      fontSize: {
+        'xxs': '0.625rem',
+      },
+      
+      spacing: {
+        '65': '17rem',
+      },
+
+      width: {
+        '81': '21rem',
       },
     },
   },
